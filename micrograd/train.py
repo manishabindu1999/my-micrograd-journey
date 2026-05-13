34d96ef2732eeb0f7d889aa1ee5d7c4bd592718f
manisha <manisha.bindu1999@gmail.com>
2026-05-13 14:24:37 +0530

    Completed micrograd implementation

--- micrograd/train.py
+++ micrograd/train.py
@@ -1,5 +1,45 @@
-from engine import Value
+from nn import MLP
 
-x = Value(2.0)
 
-print(x)
+# dataset
+xs = [
+    [2.0, 3.0, -1.0],
+    [3.0, -1.0, 0.5],
+    [0.5, 1.0, 1.0],
+    [1.0, 1.0, -1.0],
+]
+
+ys = [1.0, -1.0, -1.0, 1.0]
+
+
+# model
+model = MLP(3, [4, 4, 1])
+
+
+# training loop
+for k in range(50):
+
+    # forward pass
+    ypred = [model(x) for x in xs]
+
+    # loss
+    loss = sum((yout - ygt)**2 for ygt, yout in zip(ys, ypred))
+
+    # zero gradients
+    model.zero_grad()
+
+    # backward pass
+    loss.backward()
+
+    # update parameters
+    for p in model.parameters():
+        p.data += -0.05 * p.grad
+
+    print(k, loss.data)
+
+
+# predictions
+print("\nFinal Predictions:")
+
+for x in xs:
+    print(model(x).data)
